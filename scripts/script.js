350534f3afada9a6eb1df8094ab7d91083ddf9e3
Schifelbain <daniel.schifelbain@capgemini.com>
2024-01-12 12:27:49 -0300

    pre-stylized buttons, add divs on content

--- scripts/script.js
+++ scripts/script.js
@@ -11,23 +11,49 @@ const createButtons = () => {
         buttons.push(button);
     }
 
+    const equalsButton = document.createElement("button");
+    equalsButton.textContent = "=";
+    const classEqualsButton = "equals_button";
+    equalsButton.classList.add(classEqualsButton);
+    buttons.push(equalsButton);
+
     const operators = ["+", "-", "*", "/"];
     operators.forEach(operator => {
         const button = document.createElement("button");
-        const classButton = "operator_button";
-        button.classList.add(classButton);
         button.textContent = operator;
+        const classOperatorButton = "operator_button";
+        button.classList.add(classOperatorButton);
         buttons.push(button);
     });
 
     return buttons;
 };
 
+const createDivs = buttons => {
+    const divs = [];
+
+    const numericDiv = document.createElement("div");
+    numericDiv.classList.add("numeric_div");
+
+    buttons.forEach(button => numericDiv.appendChild(button));
+
+    const operatorDiv = document.createElement("div");
+    operatorDiv.classList.add("operator_div");
+
+    buttons.filter(button => button.classList.contains("operator_button"))
+           .forEach(button => operatorDiv.appendChild(button));
+
+    divs.push(numericDiv, operatorDiv);
+
+    return divs;
+};
+
 const createContent = () => {
     const buttons = createButtons();
+    const divs = createDivs(buttons);
 
-    buttons.forEach(button => {
-        contentGrid.appendChild(button);
+    divs.forEach(div => {
+        contentGrid.appendChild(div);
     });
 };
 
